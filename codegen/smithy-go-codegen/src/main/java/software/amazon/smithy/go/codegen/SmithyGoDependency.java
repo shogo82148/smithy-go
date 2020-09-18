69f605add286f5235fdc559d5700a32d0b50209d
skotambkar <kotambka@amazon.com>
2020-09-17 18:19:32 -0700

    update go hash

--- codegen/smithy-go-codegen/src/main/java/software/amazon/smithy/go/codegen/SmithyGoDependency.java
+++ codegen/smithy-go-codegen/src/main/java/software/amazon/smithy/go/codegen/SmithyGoDependency.java
@@ -109,6 +109,6 @@ public final class SmithyGoDependency {
     private static final class Versions {
         private static final String GO_STDLIB = "1.14";
         private static final String GO_CMP = "v0.4.1";
-        private static final String SMITHY_GO = "v0.0.0-20200917082847-627658712072";
+        private static final String SMITHY_GO = "v0.0.0-20200918011633-2653205b6c1b";
     }
 }
